2f1bdc94b89b7eb0bd1ee47bdf330d7a096c73f3
Esau21 <esaumoran1999@gmail.com>
2023-01-09 17:24:56 -0600

    Java junior Developer

---
+++ Ejemplo1.java
@@ -0,0 +1,9 @@
+public class Ejemplo1 {
+
+    /* aqui arrancamos el programa */
+    /* Tambien poner ; despues de finalizar cada linea de codigo */
+    /* Mostramos por pantalla un hola mundo */
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
