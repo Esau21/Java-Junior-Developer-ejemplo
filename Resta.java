37d471246947053e03cb3d79a0a8a9d6d4de2874
Esau21 <esaumoran1999@gmail.com>
2023-01-09 18:03:55 -0600

    Java junior Developer

---
+++ Resta.java
@@ -0,0 +1,10 @@
+public class Resta {
+    public static void main(String args[]){
+        int num_1 = 10;
+        int num_2 = 2;
+        int result = 0;
+
+        result = num_1 - num_2;
+        System.out.println("El resultado de la resta es : " + result);
+    }
+}
---
+++ Suma.java
@@ -0,0 +1,12 @@
+public class Suma {
+    /* arrancamos el programa */
+    public static void main(String args[]){
+        /* declaramos el tipo de datos y el nombre de la variable */
+        int numero_uno = 5;
+        int numero_dos = 2;
+        int result = 0;
+
+        result = numero_uno + numero_dos;
+        System.out.println("El valor de tu suma es : " + result);
+    }
+}
