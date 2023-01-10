def10f6582a138633f105d297af5692a720bf183
Esau21 <esaumoran1999@gmail.com>
2023-01-09 18:33:18 -0600

    Java junior Developer

---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    public static void main(String args[]){
+        double numero_1 = 130.56;
+        double numero_2 = 120.65;
+        double result = 0;
+
+        result = numero_1 / numero_2;
+        System.out.println("la divison para esta expreion es : " + result);
+
+    }
+}
---
+++ Multiplicacion.java
@@ -0,0 +1,16 @@
+public class Multiplicacion {
+
+    /* declaramos el tipo de variable */
+    public static void main(String args[]){
+        double numero_1 = 300.45;
+        double numero_2 = 400.56;
+        double numero_3 = 23.3;
+        double result = 0;
+
+        /* concatenamos el resultado que queremos que sea de vuelto */
+        result = numero_1 * numero_2 * numero_3;
+
+        /* Imprimos en panatalla */
+        System.out.println("El resultado para esta expresion es : " + result);
+    }
+}
